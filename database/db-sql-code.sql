34715d22302dd0e387eabe4f4ade693d234ba1a2
GPuebla <gabrielpuebla16@gmail.com>
2024-06-22 17:51:57 -0300

    Updating - assignment 1 file and creating - assignment 2 file.

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -233,3 +233,13 @@ VALUES   (
     'White',
     5
   );
+
+-- Query 4 - assignment 2
+UPDATE inventory
+SET inv_description = REPLACE(inv_description,'the small interiors','a huge interior')
+WHERE inv_id = 10;
+
+-- Query 6 - assignment 2
+UPDATE inventory
+SET inv_image = REPLACE(inv_image,'/images/','/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail,'/images/','/images/vehicles/');
